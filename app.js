ece7293b96367980c27f63b44266ac139d93af82
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-07-29 22:05:53 -0500

    added basic site (non functional)

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("I'm printing!!!");
+//Global Variables
+var IDcounter = 0; // used to keep track of && asign employee IDS
+
+
+$(document).ready(function() {
+    console.log("DOM is ready");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
