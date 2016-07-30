54de331d6eec167a77d964a76b40cd5d3fbbad68
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-07-30 11:25:49 -0500

    saves inputs to employees array

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-//Global Variables
-var IDcounter = 0; // used to keep track of && asign employee IDS
-
-
 $(document).ready(function() {
-    console.log("DOM is ready");
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	var employees = []; 																			//where the employee objects are stored
+
+	$('#employeeinfo').on('submit', function(event) { 				// prevents from trying to submit to server
+      event.preventDefault();
+
+	var values = {};																					// new employee object
+	var fields = $('#employeeinfo').serializeArray();
+
+	fields.forEach(function(element, index, array) {					// creates employee based on each field in the form
+        values[element.name] = element.value;
+      });
+	employees.push(values);																		// pushes the employee object to the employees array
+	console.log(employees);
+	 $('#employeeinfo').find('input[type=text]').val('');			// clears all the values in the form
+
+
+
+});
 });
